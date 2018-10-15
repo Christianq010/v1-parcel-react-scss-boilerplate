43080e610190d972ca7e84fc388399b826b2032f
Christiaan Quyn <christianq010@gmail.com>
2018-10-15 11:24:00 +0530

    Add styled components to component

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+import styled from 'styled-components'
+
+const App = styled.div`
+  background: palevioletred;
+  color: white;
+`
+
 export const Pet = props => {
   return (
-    <div>
+    <App>
       <h1>{props.name}</h1>
       <h2>{props.animal}</h2>
       <h2>{props.breed}</h2>
-    </div>
+    </App>
   );
 };
